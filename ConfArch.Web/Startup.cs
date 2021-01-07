befbb9f46df1ec1b6f641b58e29ea20ab2cd261d
Ladislav Margai <LadislavM@kentico.com>
2021-01-07 15:46:07 +0100

    Initial commit

---
+++ ConfArch.Web/Startup.cs
@@ -0,0 +1,107 @@
+using ConfArch.Data;
+using ConfArch.Data.Repositories;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+
+namespace ConfArch.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews(o => o.Filters.Add(new AuthorizeFilter()));
+            services.AddScoped<IConferenceRepository, ConferenceRepository>();
+            services.AddScoped<IProposalRepository, ProposalRepository>();
+            services.AddScoped<IAttendeeRepository, AttendeeRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            services.AddDbContext<ConfArchDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                    assembly => assembly.MigrationsAssembly(typeof(ConfArchDbContext).Assembly.FullName)));
+
+            services.AddAuthentication(o => { 
+                o.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                //o.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+            })
+                .AddCookie()
+                .AddCookie(ExternalAuthenticationDefaults.AuthenticationScheme)
+                .AddGoogle(o =>
+                {
+                    o.SignInScheme = ExternalAuthenticationDefaults.AuthenticationScheme;
+                    o.ClientId = Configuration["Google:ClientId"];
+                    o.ClientSecret = Configuration["Google:ClientSecret"];
+                });
+
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "admin/client/build"; // Or any other folder
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.MapWhen(context => context.Request.Path.Value.StartsWith("/admin", StringComparison.OrdinalIgnoreCase), (adminApp) =>
+            {
+                adminApp.UseSpaStaticFiles();
+
+                adminApp.UseSpa(spa =>
+                {
+                    spa.Options.SourcePath = "admin/client";
+                    spa.Options.DefaultPageStaticFileOptions = new StaticFileOptions
+                    {
+                        FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "admin", "client"))
+                    };
+
+                    if (env.IsDevelopment())
+                    {
+                        spa.UseReactDevelopmentServer(npmScript: "start");
+                    }
+                });
+            });
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Conference}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # KXO-Membership-PROTOTYPE
+1. Clone the repository
+2. In Package Manager Console run `Update-Database`
+3. Run the project (F5)
